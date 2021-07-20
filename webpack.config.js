0a50b9914301ba6a79f51e4a5a28e6fb96f0e698
Marcio Najarro <marcionajarro@gmail.com>
2021-07-20 18:26:45 -0500

    react/03

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,7 +8,7 @@ module.exports = {
         filename: 'bundle.js',
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js','.jsx']
     },
     module: {
         rules: [
@@ -17,9 +18,22 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                   { loader : 'html-loader'} 
+                ]
             }
         ]
-    },devServer: {
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 3006,
